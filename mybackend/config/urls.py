631b4b7647f693603e7a5b4e0b5911c20b6738d3
dhia sakkar <mohameddhiasakkar@gmail.com>
2026-06-21 15:46:30 +0100

    Add API health endpoint

--- mybackend/config/urls.py
+++ mybackend/config/urls.py
@@ -1,13 +1,27 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+
+def home(request):
+    return JsonResponse({
+        "status": "running",
+        "message": "Score Resume API 🚀"
+    })
+
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('core_api.urls')),
     path('api/', include('ai_engine.urls')),
 ]
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
